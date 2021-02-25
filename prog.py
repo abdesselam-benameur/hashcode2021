12ee4157ad79372f520ab29d2dae37dd78a95472
Abdesselam Benameur <ia_benameur@esi.dz>
2021-02-25 21:32:20 +0100

    First commit

---
+++ prog.py
@@ -0,0 +1,96 @@
+datasets = [
+    "a.txt",
+    "b.txt",
+    "c.txt",
+    "d.txt",
+    "e.txt",
+    "f.txt"
+]
+
+
+#%%
+class Street:
+    def __init__(self, start, end, name, duration):
+        self.name = name
+        self.start = start
+        self.end = end
+        self.duration = duration
+        self.lignt = False
+        self.cars = []
+
+    def __str__(self):
+        return "name{%s} start{%s} end{%s} duration{%s}" % (
+            self.name, self.start, self.end, self.duration)
+
+
+class Intersection:
+    def __init__(self, id):
+        self.id = id
+        self.street_on = None
+        self.streets_in = {}
+        self.streets_out = []
+
+
+class Car:
+    def __init__(self, path):
+        self.path = path
+        self.position = 0
+
+#%%
+
+# for dataset in datasets:
+for dataset in datasets:
+    streets = {}
+    cars = []
+    file = open(dataset)
+    D, I, S, V, F = list(map(int, file.readline().strip().split()))
+    intersections = [Intersection(i) for i in range(I)]
+
+    for i in range(S):
+        line = file.readline().strip().split()
+        start = intersections[int(line[0])]
+        end = intersections[int(line[1])]
+        name = line[2]
+        street = Street(start, end, name, int(line[3]))
+        streets[name] = street
+        start.streets_out.append(street)
+        end.streets_in[street.name] = 0
+
+    for i in range(V):
+        line = file.readline().strip().split()[1:]
+        path = []
+        for street in line:
+            path.append(streets[street])
+        car = Car(path)
+        cars.append(car)
+        path[0].cars.append(car)
+
+    file.close()
+
+    # %%
+
+    # for intersection in intersections:
+    #     if intersection.street_on != None:
+    #         street_cars = streets[intersection.street_on].cars
+    #         if len(street_cars) != 0:
+    #             car = street_cars.pop(0)
+    #             if car.position < len(car.path):
+    #                 car.position += 1
+
+    #%%
+
+    # with open('intersecations.txt', 'w') as file:
+    #     for i in range(I):
+    #         print(i, file=file)
+    #         print(*intersections[i].streets_in, file=file)
+    #         print(*list(map(lambda e: e.name, intersections[i].streets_out)), file=file)
+
+
+    with open('submissions/{}'.format(dataset), 'w') as file:
+        print(I, file=file)
+        for i in range(I):
+            print(i, file=file)
+            streets_in = intersections[i].streets_in
+            print(len(streets_in), file=file)
+            for street in streets_in:
+                print(street, 2, file=file)
